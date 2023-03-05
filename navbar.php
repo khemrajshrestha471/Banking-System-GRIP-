a5a395c0a1fcdab84f7ac95300b2b268614a76c6
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-03-05 23:09:51 +0545

    first commit

---
+++ navbar.php
@@ -0,0 +1,37 @@
+<!-- navbar for all page is common -->
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="https://www.thesparksfoundationsingapore.org/" target="_blank">GRIP</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Add User</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="transaction.php">Transaction</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="deposite.php">Deposite</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="viewdata.php">View Dataset</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="https://www.khemrajshrestha.com.np/" target="_bla
+                    ">My Portfolio</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
