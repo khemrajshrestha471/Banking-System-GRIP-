17519269fbee1c39b802029cd121a56f7180d57f
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-03-06 11:55:28 +0545

    link updated

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="https://www.thesparksfoundationsingapore.org/" target="_blank">GRIP</a>
+        <a class="navbar-brand" href="https://internship.thesparksfoundation.info/" target="_blank">GRIP</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
